794e260baaf7211b676ce858a7d787b5b35106ef
tux1500 <62507051+tux1500@users.noreply.github.com>
2023-03-31 15:37:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-j73j3r
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-j73j3r)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify(product: Product) {
+    window.alert('We will notify you when -' + product.name + '- is on sale');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/

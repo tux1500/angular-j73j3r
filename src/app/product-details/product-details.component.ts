735356abea89bbcb8fe6470f97f5dac22cee3c6e
tux1500 <62507051+tux1500@users.noreply.github.com>
2023-04-17 17:49:18 -0400

    get product id from route info and show product details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product, products } from '../products';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent {
+  product : Product | undefined;
+
+  constructor(private route : ActivatedRoute) {
+  }
+
+  private ngOnInit() {
+    //get product id from the current route
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    //find product corresponding to the product id
+    this.product = products.find(p => p.id === productIdFromRoute);
+
+  }
 
 }
